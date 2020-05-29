c3a828e9fffc87ef6e13e66eb736e88807b4266c
Alessio Coser <alessio.coser@xpeppers.com>
2020-05-29 15:29:07 +0200

    handled generic objects

--- src/main/kotlin/topinambur/gson/ServerResponseExtensions.kt
+++ src/main/kotlin/topinambur/gson/ServerResponseExtensions.kt
@@ -1,8 +1,13 @@
 package topinambur.gson
 
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import topinambur.ServerResponse
 
 inline fun <reified T : Any> ServerResponse.json(gsonBuilder: GsonBuilder = GsonBuilder()): T {
-    return gsonBuilder.create().fromJson(body, T::class.java)
+    return gsonBuilder.create().fromJson(body, object: TypeToken<T>() {}.type)
+}
+
+inline fun <reified T : Any> ServerResponse.json(vararg deserializers: Deserializer<*>): T {
+    return json(GsonBuilder().apply { deserializers.forEach { registerTypeAdapter(it.type.java, it) } })
 }
--- src/test/kotlin/topinambur/gson/ServerResponseJsonTest.kt
+++ src/test/kotlin/topinambur/gson/ServerResponseJsonTest.kt
@@ -1,10 +1,16 @@
 package topinambur.gson
 
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import topinambur.ServerResponse
+import java.time.LocalDateTime
+import java.time.LocalDateTime.parse
 
+data class TimeResponse(val time: LocalDateTime)
 data class TestResponse(val code: String = "")
 
 class ServerResponseJsonTest {
@@ -30,5 +36,36 @@ class ServerResponseJsonTest {
         assertThrows<IllegalStateException> {
             response.json<TestResponse>()
         }
+    }
+
+    @Test
+    fun `valid list of json strings`() {
+        val response = ServerResponse(200, """[{"code":"first"},{"code":"second"}]""", emptyMap())
+
+        assertThat(response.json<List<TestResponse>>()).isEqualTo(listOf(TestResponse("first"), TestResponse("second")))
+    }
+
+    @Test
+    fun `valid list of json datetime`() {
+        val response = ServerResponse(200, """[{"time":"2020-01-01T12:00:00"},{"time":"2020-03-03T23:59:59"}]""", emptyMap())
+
+        val builder = GsonBuilder().registerTypeAdapter(LocalDateTime::class.java, JsonDeserializer { json: JsonElement, _, _ -> parse(json.asString) })
+
+        assertThat(response.json<List<TimeResponse>>(builder)).isEqualTo(listOf(
+                TimeResponse(parse("2020-01-01T12:00:00")),
+                TimeResponse(parse("2020-03-03T23:59:59"))
+        ))
+    }
+
+    @Test
+    fun `valid list of json datetime using alternative json method`() {
+        val response = ServerResponse(200, """[{"time":"2020-01-01T12:00:00"},{"time":"2020-03-03T23:59:59"}]""", emptyMap())
+
+        val deserializer = Deserializer(LocalDateTime::class) { json: JsonElement, _, _ -> parse(json.asString) }
+
+        assertThat(response.json<List<TimeResponse>>(deserializer)).isEqualTo(listOf(
+                TimeResponse(parse("2020-01-01T12:00:00")),
+                TimeResponse(parse("2020-03-03T23:59:59"))
+        ))
     }
 }
