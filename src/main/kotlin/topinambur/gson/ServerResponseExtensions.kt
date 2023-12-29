04d53709a676302ea01eeaf05d1104f6747a251f
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2023-12-30 00:57:45 +0100

    support generic json parsing

--- src/main/kotlin/topinambur/gson/ServerResponseExtensions.kt
+++ src/main/kotlin/topinambur/gson/ServerResponseExtensions.kt
@@ -1,11 +1,27 @@
 package topinambur.gson
 
-import com.google.gson.GsonBuilder
+import com.google.gson.*
 import com.google.gson.reflect.TypeToken
 import topinambur.ServerResponse
 
+fun ServerResponse.jsonArray(): JsonArray? {
+    return jsonElement()?.asJsonArray
+}
+
+fun ServerResponse.jsonObject(): JsonObject? {
+    return jsonElement()?.asJsonObject
+}
+
+fun ServerResponse.jsonElement(): JsonElement? {
+    return JsonParser.parseString(body)
+}
+
 inline fun <reified T : Any> ServerResponse.json(gsonBuilder: GsonBuilder = GsonBuilder()): T {
-    return gsonBuilder.create().fromJson(body, object: TypeToken<T>() {}.type)
+    try {
+        return gsonBuilder.create().fromJson(body, object : TypeToken<T>() {}.type)
+    } catch (e: NullPointerException) {
+        throw IllegalStateException("Cannot convert to ${T::class.java} class. Json: '$body'")
+    }
 }
 
 inline fun <reified T : Any> ServerResponse.json(vararg deserializers: Deserializer<*>): T {
--- src/test/kotlin/topinambur/gson/ServerResponseJsonTest.kt
+++ src/test/kotlin/topinambur/gson/ServerResponseJsonTest.kt
@@ -17,37 +17,39 @@ class ServerResponseJsonTest {
 
     @Test
     fun `Empty object`() {
-        val response = ServerResponse(200, "{}", emptyMap())
+        val response = ServerResponse(200, "{}".toByteArray(), emptyMap())
 
         assertThat(response.json<TestResponse>()).isEqualTo(TestResponse(""))
     }
 
     @Test
     fun `valid json string`() {
-        val response = ServerResponse(200, """{"code":"prova"}""", emptyMap())
+        val response = ServerResponse(200, """{"code":"prova"}""".toByteArray(), emptyMap())
 
         assertThat(response.json<TestResponse>()).isEqualTo(TestResponse("prova"))
     }
 
     @Test
     fun `invalid json string`() {
-        val response = ServerResponse(200, """""", emptyMap())
+        val response = ServerResponse(200, "".toByteArray(), emptyMap())
 
-        assertThrows<IllegalStateException> {
+        val errorMessage = assertThrows<IllegalStateException> {
             response.json<TestResponse>()
-        }
+        }.message
+
+        assertThat(errorMessage).isEqualTo("Cannot convert to class topinambur.gson.TestResponse class. Json: ''")
     }
 
     @Test
     fun `valid list of json strings`() {
-        val response = ServerResponse(200, """[{"code":"first"},{"code":"second"}]""", emptyMap())
+        val response = ServerResponse(200, """[{"code":"first"},{"code":"second"}]""".toByteArray(), emptyMap())
 
         assertThat(response.json<List<TestResponse>>()).isEqualTo(listOf(TestResponse("first"), TestResponse("second")))
     }
 
     @Test
     fun `valid list of json datetime`() {
-        val response = ServerResponse(200, """[{"time":"2020-01-01T12:00:00"},{"time":"2020-03-03T23:59:59"}]""", emptyMap())
+        val response = ServerResponse(200, """[{"time":"2020-01-01T12:00:00"},{"time":"2020-03-03T23:59:59"}]""".toByteArray(), emptyMap())
 
         val builder = GsonBuilder().registerTypeAdapter(LocalDateTime::class.java, JsonDeserializer { json: JsonElement, _, _ -> parse(json.asString) })
 
@@ -59,7 +61,7 @@ class ServerResponseJsonTest {
 
     @Test
     fun `valid list of json datetime using alternative json method`() {
-        val response = ServerResponse(200, """[{"time":"2020-01-01T12:00:00"},{"time":"2020-03-03T23:59:59"}]""", emptyMap())
+        val response = ServerResponse(200, """[{"time":"2020-01-01T12:00:00"},{"time":"2020-03-03T23:59:59"}]""".toByteArray(), emptyMap())
 
         val deserializer = Deserializer(LocalDateTime::class) { json: JsonElement, _, _ -> parse(json.asString) }
 
@@ -67,5 +69,33 @@ class ServerResponseJsonTest {
                 TimeResponse(parse("2020-01-01T12:00:00")),
                 TimeResponse(parse("2020-03-03T23:59:59"))
         ))
+    }
+
+    @Test
+    fun `parse to json array`() {
+        val response = ServerResponse(200, """[{"code":"first"},{"code":"second"}]""".toByteArray(), emptyMap())
+
+        val json = response.jsonArray()!!
+
+        assertThat(json.get(0).asJsonObject.get("code").asString).isEqualTo("first")
+        assertThat(json.get(1).asJsonObject.get("code").asString).isEqualTo("second")
+    }
+
+    @Test
+    fun `parse to json object`() {
+        val response = ServerResponse(200, """{"code":"first"}""".toByteArray(), emptyMap())
+
+        val json = response.jsonObject()!!
+
+        assertThat(json.get("code").asString).isEqualTo("first")
+    }
+
+    @Test
+    fun `parse to json element`() {
+        val response = ServerResponse(200, "\"first\"".toByteArray(), emptyMap())
+
+        val json = response.jsonElement()!!
+
+        assertThat(json.asString).isEqualTo("first")
     }
 }
